1974a6151904067a0d439cdaef4568bef67a8c23
steerenergy <36624165+steerenergy@users.noreply.github.com>
2018-06-06 14:47:18 +0100

    A Quick quiz

---
+++ codingIsFun.py
@@ -0,0 +1,21 @@
+# This is a Quick Test
+# Shared Vars: name = Persons Name, age = persons age
+
+
+# Tom
+# Create function that asks for users name and DOB year and prints age
+
+
+
+
+# Nick
+# Create function which gets the name of the persons favourite football team and tells them what you think of them
+# When you tell them the opinion of the football team, address the person using the name they gave in the first part
+
+
+
+
+# John
+# Interpret a 920,232 MB CSV file and plot this on a 4D Graph while figuring out the meaning of life
+# Nah just kidding
+# Create a function which calculates the circumference and area of a circle with the Diameter being the age from part 1
