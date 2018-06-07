470f650df9eaab8df812894b67d0727d036a7b54
Jon <jon@steerenergy.com>
2018-06-07 11:46:14 +0100

    Edit comment to make capitalised

--- codingIsFun.py
+++ codingIsFun.py
@@ -17,6 +17,6 @@
 
 
 # John
-# Interpret a 920,232 MB CSV file and plot this on a 4D Graph while figuring out the meaning of life
+# INTERPRET a 920,232 MB CSV file and plot this on a 4D Graph while figuring out the meaning of life
 # Nah just kidding
 # Create a function which calculates the circumference and area of a circle with the diameter being the age from part 1
