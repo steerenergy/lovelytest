2cc793472dff6215b132b9b0eb4d5c84ff1a0aca
NickRyanSteer <36924254+NickRyanSteer@users.noreply.github.com>
2018-06-19 17:20:13 +0100

    Done my bit of coding

--- codingIsFun.py
+++ codingIsFun.py
@@ -3,18 +3,33 @@
 
 
 # Tom
-# Create function that asks for users name and DOB year and prints age
+# Create function that asks for users name and DIB year and prints age
 
-
+# add in name for toms bit
+name = input("What is your name? ")
 
 
 # Nick
 # Create function which gets the name of the persons favourite football team and tells them what you think of them
 # When you tell them the opinion of the football team, address the person using the name they gave in the first part
 # (it's polite y'know)
-foot_name = input("Please type in the name of your favourite football team")
 
-
+# Introduce yourself
+print("Hey {}, \n".format(name))
+# Ask for favourite footy team
+foot_name = input("Please type in the name of your favourite football team ")
+# print first response
+print("What {}, really?\n".format(foot_name))
+print("Honestly,", foot_name, "... let me think")
+# print length of string count
+print(foot_name, "has", len(foot_name), "letters in it.")
+# convert to lower case
+# foot_name = foot_name.lower()
+# comment on their choice of team
+if foot_name.lower() == "formby squirrels":
+    print("Great Choice {} good old {} are my fave too".format(name, foot_name))
+else:
+    print("Looser, looser", name, "is a looser", foot_name, "are pants!")
 
 # John
 # Interpret a 920,232 MB CSV file and plot this on a 4D Graph while figuring out the meaning of life
