d441521c627abaf440bbcec20fdd4566b6ca5b9b
Jon <jon@steerenergy.com>
2018-06-20 09:16:11 +0100

    Added some killer humour
    Note this script wants to pick up Tom's age variable so on it's own is broke.

--- codingIsFun.py
+++ codingIsFun.py
@@ -17,24 +17,33 @@
 
 
 # John
-# INTERPRET a 920,232 MB CSV file and plot this on a 4D Graph while figuring out the meaning of life
-# Nah just kidding
 # Create a function which calculates the circumference and area of a circle with the diameter being the age from part 1
 import math
-pi = math.pi
-print("For no good reason, we're going to calculate some properties of a circle with a diameter equal to your age and devoid of units")
+import time
 
+pi = math.pi
+print("For no good reason, we're going to calculate some properties of a circle with a diameter equal to your age"
+          "\n ... and devoid of units.")
+time.sleep(2)
+print("Let the electrons get up to speed ...")
+time.sleep(2)
+print("... mmm ... mmm ... difficult ... bit of a sorting hat moment ...")
+time.sleep(2)
 
 if age <= 0:
     print("I'm going to take the legal position that you're age zero, given you've not been born yet.")
+    time.sleep(1)
     print("Which means there is no circle. End of.")
 elif 0 < age:
     print("OK, here we go then ...")
+    time.sleep(1)
     dia = age
     circ = pi * age
     area = pi / 4 * age ** 2
     print("A circle with a diameter equal to your age of ", age," has a circumference of",circ," and an area of ", area)
+    time.sleep(2)
+    print("Go and make a coffee and calm down after all that excitement")
 else:
-    print("However, given you have an indecipherable age, we'll just go make a coffee ...")
+    print("However, given you have an indecipherable age, we'll just go make a coffee ... numpty")
 
 
