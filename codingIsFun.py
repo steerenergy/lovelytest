3c237b90401594de0139bbb17835b3ad458e5993
Jon <jon@steerenergy.com>
2018-06-20 09:05:57 +0100

    Bare bones logic flow in place with calcs

--- codingIsFun.py
+++ codingIsFun.py
@@ -22,9 +22,19 @@
 # Create a function which calculates the circumference and area of a circle with the diameter being the age from part 1
 import math
 pi = math.pi
-age = 5.4
-dia = age
-circ = pi * age
-area = pi / 4 * age**2
+print("For no good reason, we're going to calculate some properties of a circle with a diameter equal to your age and devoid of units")
+
+
+if age <= 0:
+    print("I'm going to take the legal position that you're age zero, given you've not been born yet.")
+    print("Which means there is no circle. End of.")
+elif 0 < age:
+    print("OK, here we go then ...")
+    dia = age
+    circ = pi * age
+    area = pi / 4 * age ** 2
+    print("A circle with a diameter equal to your age of ", age," has a circumference of",circ," and an area of ", area)
+else:
+    print("However, given you have an indecipherable age, we'll just go make a coffee ...")
 
 
