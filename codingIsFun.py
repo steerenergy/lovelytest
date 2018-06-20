7a0ef41d0a3d0f99b9047a10c99faf7f2606f3df
Jon <jon@steerenergy.com>
2018-06-20 08:52:04 +0100

    Getting started with Jon's element

--- codingIsFun.py
+++ codingIsFun.py
@@ -20,3 +20,11 @@
 # INTERPRET a 920,232 MB CSV file and plot this on a 4D Graph while figuring out the meaning of life
 # Nah just kidding
 # Create a function which calculates the circumference and area of a circle with the diameter being the age from part 1
+import math
+pi = math.pi
+age = 5.4
+dia = age
+circ = pi * age
+area = pi / 4 * age**2
+
+
